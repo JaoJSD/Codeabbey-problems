ad96b8aeeae7c9b8d7937448e729b93df130dc20
jair socarras <jjsd204@gmail.com>
2023-10-12 21:37:54 -0500

    Update minimumOfThree.lua

--- minimumOfThree.lua
+++ minimumOfThree.lua
@@ -1,10 +1,30 @@
 count = io.read()
-for i = 1, count do    
-  vals = io.read()
-  local num1, num2, num3 = vals:match("([%-?%d]+)%s+([%-?%d]+)%s+([%-?%d]+)")
-  num1 = tonumber(num1)
-  num2 = tonumber(num2)
-  num3 = tonumber(num3)
-  local minOfThree = math.min(num1, num2, num3)
-  print(minOfThree)  
+count1 = tonumber(count)
+first = 1
+list1 = {}
+
+function arrayNum(number, countTotal, list)    
+    if number <= countTotal then do
+        eChain = io.read()        
+        list[number] = getChainNum(eChain)
+        print(list[number])
+        arrayNum(number + 1, countTotal, list)
+        end
+    end
 end
+
+function getChainNum(chain)    
+  local const_a, const_b, const_c = chain:match("([%-?%d]+)%s+([%-?%d]+)%s+([%-?%d]+)")
+    const_a = tonumber(const_a)
+    const_b = tonumber(const_b)
+    const_c = tonumber(const_c)
+    result = setMinOfThree(const_a, const_b, const_c)
+return result
+end
+
+function setMinOfThree(num1, num2, num3)
+  local minOfThree = math.min(num1, num2, num3)
+return minOfThree
+end
+
+arrayNum(first, count1, list1)
