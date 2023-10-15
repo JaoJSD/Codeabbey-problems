8ea034682b8558ef9043c0ceca11e4c487f4d548
jair socarras <jjsd204@gmail.com>
2023-10-14 23:36:11 -0500

    Update minimumOfThree.lua

--- minimumOfThree.lua
+++ minimumOfThree.lua
@@ -5,17 +5,17 @@ list1 = {}
 
 function arrayNum(number, countTotal, list)    
     if number <= countTotal then do
-        eChain = io.read()        
-        table.insert(list, getChainNum(eChain))        
+        eChain = io.read()
+        table.insert(list, number, getChainNum(eChain))
         arrayNum(number + 1, countTotal, list)
-        end
+        end        
     end
     return list
 end
 
 function getChainNum(chain)    
   local const_a, const_b, const_c = chain:match("([%-?%d]+)%s+([%-?%d]+)%s+([%-?%d]+)")
-    const_a = tonumber(const_a)
+    const_a = tonumber(const_a)    
     const_b = tonumber(const_b)
     const_c = tonumber(const_c)
     result = setMinOfThree(const_a, const_b, const_c)
@@ -27,12 +27,5 @@ function setMinOfThree(num1, num2, num3)
 return minOfThree
 end
 
-function printing_list(cter1, totalCter, lst1)
-  if cter1 <= totalCter then
-    print(lst1[cter1])   
-    printing_list(cter1 + 1, totalCter, lst1)
-  end
-end
-
-table.insert(list1, arrayNum(first, count1, list1))
-printing_list(first, count1, list1)
+list1 = arrayNum(first, count1, list1)
+print(table.concat(list1, ' '))
